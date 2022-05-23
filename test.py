9709c60ff665dd29fdd0a7cfb69c243c5904c538
I Gede Govindabhakta <igovindabhakta@gmail.com>
2022-05-23 19:50:33 +0700

    Added new model placeholder

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-from pocketsphinx import LiveSpeech
+from pocketsphinx import LiveSpeech, get_model_path, get_data_path
+
+print(get_model_path())
+print(get_data_path())
 
 for phrase in LiveSpeech():
     print(phrase)
